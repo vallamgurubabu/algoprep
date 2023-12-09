98255a44142cb2fa4696ebfce1203d7690eb9c39
gurubabu vallam <vallamgurubabu@gmail.com>
2023-12-09 09:57:37 +0530

    done

--- loop/print_prime.java
+++ loop/print_prime.java
@@ -15,7 +15,7 @@ public class print_prime {
     }
     public static boolean isprime(int num){
         int flag = 0;
-         for(int i =2;i<num;i++){
+         for(int i =2;i<num;){
           if(num%i==0){
              flag=1;
           }
